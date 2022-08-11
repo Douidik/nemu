c8192b6a6afb3aec1739fc7e070102f89a550126
douidik <douidikjam@gmail.com>
2022-08-11 22:54:09 +0200

    Output directory updated to bin/lib

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,5 +1,5 @@
 set(CPM_VERSION 0.35.0)
-set(CPM_FILEPATH "${NEMU_ROOT}/cmake/cpm_${CPM_VERSION}.cmake")
+set(CPM_FILEPATH "${NEMU_ROOT}/cmake/cpm.cmake")
 
 if(NOT EXISTS ${CPM_SOURCE_FILEPATH})
   file(DOWNLOAD https://github.com/TheLartians/CPM.cmake/releases/download/v${CPM_VERSION}/CPM.cmake ${CPM_FILEPATH})
